86c052cd05a8194e0ad8480409c711fb12eac907
Philip Arola <parola@pdx.edu>
2018-10-28 13:49:54 -0700

    Tested Arduino sketch import capability

---
+++ src/arduino_import_test/arduino_import_test/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 10/28/2018 1:46:10 PM
+ * Author : parola
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
