82147d823f053ef67238de5e93f1eba63b87cf54
Philip Arola <parola@pdx.edu>
2018-10-28 13:11:47 -0700

    Created project
    GCC C++ Executable Project
    Target: ATmega48A

---
+++ src/voice-modulator/voice-modulator/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * voice-modulator.cpp
+ *
+ * Created: 10/28/2018 1:10:05 PM
+ * Author : parola
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
