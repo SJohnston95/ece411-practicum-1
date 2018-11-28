4ce0ca30cfa610b11b11cc4720538de962c0782d
Aaron <chanaar@pdx.edu>
2018-11-27 21:02:55 -0800

    Successful test code that works with Arduino Nano
    Uses new DAC library. Tested and works.

---
+++ src/arduino_dac/testdac/testdac.ino
@@ -0,0 +1,17 @@
+#include <MCP492X.h>
+
+#define CS_DAC 5 // control chip select on DAC
+
+MCP492X myDac(CS_DAC);
+
+void setup() {
+  // put your setup code here, to run once:
+  myDac.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  long data;
+  data = analogRead(A0);
+  myDac.analogWrite(0,0,0,1,data);
+}
