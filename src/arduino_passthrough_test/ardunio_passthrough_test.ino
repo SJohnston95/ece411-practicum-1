cb62a244228b1e535e77e65563796b04a53c0e62
Jordan <jordanb716@gmail.com>
2018-12-02 15:24:27 -0800

    Cleanup and new arduino_passthrough_test folder.
    Did some cleanup of old files and started a new project folder, ADC library I was testing didn't work out.

---
+++ src/arduino_passthrough_test/ardunio_passthrough_test.ino
@@ -0,0 +1,18 @@
+#include <MCP492X.h>
+
+#define CS_DAC 5 // control chip select on DAC
+
+MCP492X myDac(CS_DAC);
+
+void setup() {
+  // put your setup code here, to run once:
+  myDac.begin();
+  analogReference(INTERNAL);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  unsigned int data;
+  data = analogRead(A0);
+  myDac.analogWrite(0,0,0,1,data);
+}
